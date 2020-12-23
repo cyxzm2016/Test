6400f58165431b4b6862fbcdb4efefaf2f528625
caoyuxi <742726997@qq.com>
2020-12-23 14:52:30 +0800

    上传

---
+++ src/main/java/test/testcom1/TestCom3.java
@@ -0,0 +1,12 @@
+package test.testcom1;
+
+public class TestCom3 {
+
+    public void cyx2Hello(){
+        System.out.println("你好cyx2Hello");
+    }
+    public void cyx3Hello(){
+        int i= 1/0;
+        System.out.println("你好cyx2Hello");
+    }
+}
---
+++ src/test/java/test/testcom1/TestCom3Test.java
@@ -0,0 +1,16 @@
+package test.testcom1;
+
+import junit.framework.TestCase;
+
+public class TestCom3Test extends TestCase {
+
+    public void testCyx2Hello() {
+        TestCom3 testCom3 = new TestCom3();
+        testCom3.cyx2Hello();
+    }
+
+    public void testCyx3Hello() {
+        TestCom3 testCom3 = new TestCom3();
+        testCom3.cyx3Hello();
+    }
+}
